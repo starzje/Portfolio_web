fb463ecd1f1edb692fe53444a9d91410c7cad91a
Ivan Starčević <istarcevic16@gmail.com>
2022-09-29 14:15:50 +0200

    contact remove test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,29 @@
-import { motion } from "framer-motion";
-import Form from "./Form";
-import { contactTitle } from "../helpers/animations";
-
 const Contact = () => {
   return (
-    <section className="relative bg-[url('./assets/footer_bg.png')] fp-auto-height bg-cover md:bg-contain bg-no-repeat  bg-center md:bg-right-bottom bg-[#140057] min-h-full md:min-h-screen  fullheight  ">
-      <div className="container flex flex-col justify-center items-start fullheight md:h-screen ">
-        <motion.h3
-          variants={contactTitle}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          className="text-[2rem] md:text-5xl text-white font-bold mb-2">
-          Like what you see?
-        </motion.h3>
-        <Form />
-      </div>
-    </section>
+    <div>
+      contact Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Perspiciatis, in. Sed suscipit ratione animi accusamus, quibusdam facere
+      quo nihil facilis reprehenderit distinctio, dolor consectetur consequuntur
+      laboriosam natus ad sequi velit labore hic cumque recusandae pariatur id
+      ducimus est. Corrupti reiciendis praesentium totam temporibus perferendis,
+      consectetur assumenda. Blanditiis unde voluptatibus ducimus modi placeat
+      laudantium! Corrupti impedit, voluptates voluptas qui id nulla ab nobis
+      ducimus atque debitis nostrum non officia accusantium vero amet ipsa esse
+      eos nam! Necessitatibus eligendi amet sapiente alias eveniet laudantium
+      labore cupiditate ex perspiciatis dicta nesciunt eos, qui, ducimus
+      voluptates! Aperiam sint quod beatae, esse ut ab quia labore delectus
+      consectetur, odio quos eaque corporis corrupti voluptatum, doloribus nihil
+      ea tempore deserunt quaerat quidem inventore! Consequuntur repudiandae
+      odio quaerat, nemo voluptatibus veritatis maxime magni sapiente quas
+      doloribus praesentium fugit tempora sed provident blanditiis, amet eius
+      maiores cumque. Aperiam minima, in praesentium earum voluptatibus non, qui
+      aliquam, porro nulla corrupti ducimus! Cum nemo odio, similique quam minus
+      totam harum, ipsa, earum consequatur soluta inventore! Placeat, voluptas.
+      Ratione, ut nihil saepe error voluptatem odio eos totam id, impedit
+      distinctio repellendus quos illo cupiditate accusamus suscipit aspernatur
+      vitae veniam dicta. Aperiam, ipsum! Alias maxime quisquam pariatur
+      voluptate aliquid esse magnam facere.
+    </div>
   );
 };
 
