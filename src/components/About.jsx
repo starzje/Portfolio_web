e0bfdcacb11b61716b64513f9c42a68ccaa78396
Ivan Starčević <istarcevic16@gmail.com>
2022-09-29 13:49:53 +0200

    deploy fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,3 @@
-import { motion } from "framer-motion";
-import {
-  aboutMeAnimation,
-  aboutMeParagraph,
-  aboutMeImage,
-} from "../helpers/animations";
-
 const About = () => {
   return <div>About me</div>;
 };
