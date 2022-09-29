4b1714f4adaec89f25bd50045feb8c3b232e4950
Ivan Starčević <istarcevic16@gmail.com>
2022-09-29 14:37:43 +0200

    form test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,13 @@
-const Contact = () => {
+import Form from "./Form";
+
+const Projects = () => {
   return (
-    <div>
-      contact Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Perspiciatis, in. Sed suscipit ratione animi accusamus, quibusdam facere
-      quo nihil facilis reprehenderit distinctio, dolor consectetur consequuntur
-      laboriosam natus ad sequi velit labore hic cumque recusandae pariatur id
-      ducimus est. Corrupti reiciendis praesentium totam temporibus perferendis,
-      consectetur assumenda. Blanditiis unde voluptatibus ducimus modi placeat
-      laudantium! Corrupti impedit, voluptates voluptas qui id nulla ab nobis
-      ducimus atque debitis nostrum non officia accusantium vero amet ipsa esse
-      eos nam! Necessitatibus eligendi amet sapiente alias eveniet laudantium
-      labore cupiditate ex perspiciatis dicta nesciunt eos, qui, ducimus
-      voluptates! Aperiam sint quod beatae, esse ut ab quia labore delectus
-      consectetur, odio quos eaque corporis corrupti voluptatum, doloribus nihil
-      ea tempore deserunt quaerat quidem inventore! Consequuntur repudiandae
-      odio quaerat, nemo voluptatibus veritatis maxime magni sapiente quas
-      doloribus praesentium fugit tempora sed provident blanditiis, amet eius
-      maiores cumque. Aperiam minima, in praesentium earum voluptatibus non, qui
-      aliquam, porro nulla corrupti ducimus! Cum nemo odio, similique quam minus
-      totam harum, ipsa, earum consequatur soluta inventore! Placeat, voluptas.
-      Ratione, ut nihil saepe error voluptatem odio eos totam id, impedit
-      distinctio repellendus quos illo cupiditate accusamus suscipit aspernatur
-      vitae veniam dicta. Aperiam, ipsum! Alias maxime quisquam pariatur
-      voluptate aliquid esse magnam facere.
-    </div>
+    <section
+      className="section relative fp-auto-height 
+    bg-bg-pattern  min-h-full  md:min-h-screen  bg-contain bg-left-top bg-no-repeat ">
+      <Form />
+    </section>
   );
 };
 
-export default Contact;
+export default Projects;
