5f913bc43757eb555fcc51afd2fcd3e010c19a83
Ivan Starčević <istarcevic16@gmail.com>
2022-09-29 13:41:33 +0200

    project test

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,34 +1,4 @@
-import TechStack from "./TechStack";
-import { motion } from "framer-motion";
-import { FiExternalLink } from "react-icons/fi";
-import { BiCodeAlt } from "react-icons/bi";
-import {
-  githubCTA,
-  projectTitle,
-  mockupImage,
-  projectDescription,
-} from "../helpers/animations";
-
 const Projects = () => {
-  const techStack = [
-    {
-      icon: "devicon-react-original",
-      iconName: "React",
-    },
-    {
-      icon: "devicon-redux-original",
-      iconName: "Redux",
-    },
-    {
-      icon: "devicon-firebase-plain",
-      iconName: "Firebase",
-    },
-    {
-      icon: "devicon-tailwindcss-plain",
-      iconName: "Tailwind",
-    },
-  ];
-
   return <div>projects</div>;
 };
 
