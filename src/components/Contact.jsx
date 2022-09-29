110a3985174904fff322a84c43598dc05f66fb19
Ivan Starčević <istarcevic16@gmail.com>
2022-09-29 13:56:10 +0200

    screen issue debugging

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,30 @@
 const Contact = () => {
-  return <div>contact</div>;
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
+      dolorum, perspiciatis velit in illum cumque explicabo suscipit ratione
+      aliquam maiores provident iusto ipsa. Ut odio magni velit excepturi
+      corrupti ratione voluptas, sunt repellendus pariatur distinctio quas neque
+      libero voluptatum non architecto reprehenderit necessitatibus accusamus,
+      vero ullam assumenda molestias. Dolorem maxime quam doloribus veritatis
+      eos, at et natus possimus sint adipisci modi harum similique ut corporis
+      ab temporibus quisquam sunt quas praesentium? Temporibus aut accusantium
+      ut in pariatur neque maiores dicta voluptatum molestiae, doloribus non.
+      Dolor eum praesentium eaque eligendi? Harum aliquam hic fugit maiores.
+      Officiis amet ut neque magnam minima nobis fuga dolorum! Nobis repellendus
+      quas expedita fugiat quibusdam iusto blanditiis velit tempore dolore
+      similique atque maxime sunt magni consequuntur quae architecto adipisci
+      voluptatem id, magnam, minus sapiente suscipit, cupiditate cum? Autem
+      magnam iure accusamus, suscipit nulla facilis, voluptatum quas unde
+      deserunt vitae atque ratione commodi beatae repudiandae tenetur molestiae
+      odit inventore at laborum, libero voluptate dolor totam id. Nam distinctio
+      officia ad tempore ratione eius et est vel perspiciatis, fuga provident
+      accusantium quibusdam autem quas accusamus libero quia, nesciunt
+      reiciendis dolore totam architecto explicabo sapiente dolorum? Quo
+      molestias vitae sapiente architecto aspernatur sequi. Aperiam a quae
+      beatae harum maiores!
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,18 @@
 const Hero = ({ fullpageApi }) => {
-  return <div className="min-h-screen">hero</div>;
+  return (
+    <div className="min-h-screen">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error sequi ipsa
+      incidunt, corporis a rerum animi accusantium. Ducimus fugit mollitia sit,
+      odit atque magni repellat labore ipsum quas incidunt amet fugiat molestias
+      ipsa, distinctio officiis pariatur, minus ad impedit illo velit cum.
+      Fugiat, dolor ratione earum consectetur eligendi nihil beatae incidunt
+      quia natus quis velit vitae odio voluptate ab est. Nostrum modi saepe sint
+      odio. Aliquam illum quasi tempora nesciunt accusamus nihil impedit nulla
+      perferendis. Laborum velit possimus tenetur officiis consectetur et atque
+      esse quos libero expedita tempore officia asperiores, pariatur sunt dolor
+      reprehenderit eaque nesciunt ex eius a inventore.
+    </div>
+  );
 };
 
 export default Hero;
